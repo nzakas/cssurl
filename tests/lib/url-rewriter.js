24f4874e9ea811bda10c70c0c20b0630e008d5d3
Kiarash <kiarash@si-co.net>
2017-12-14 23:47:14 +0100

    * Add ability to surround links with quotes * Add tests * Update dependencies

--- tests/lib/url-rewriter.js
+++ tests/lib/url-rewriter.js
@@ -15,20 +15,20 @@ var assert = require("chai").assert,
 	fs = require("fs"),
 	URLRewriter = require("../../lib/url-rewriter");
 
-describe("URLRewriter", function() {
+describe("URLRewriter", function () {
 
-	describe("new URLRewriter", function() {
+	describe("new URLRewriter", function () {
 
-		it("should throw an error when the first argument is missing", function() {
-			assert.throws(function() {
+		it("should throw an error when the first argument is missing", function () {
+			assert.throws(function () {
 				/*eslint-disable no-unused-vars*/
 				var rewriter = new URLRewriter();
 				/*eslint-enable no-unused-vars*/
 			}, /Constructor expects a function/);
 		});
 
-		it("should throw an error when the first argument isn't a function", function() {
-			assert.throws(function() {
+		it("should throw an error when the first argument isn't a function", function () {
+			assert.throws(function () {
 				/*eslint-disable no-unused-vars*/
 				var rewriter = new URLRewriter("hallo");
 				/*eslint-enable no-unused-vars*/
@@ -37,10 +37,10 @@ describe("URLRewriter", function() {
 
 	});
 
-	describe("rewrite()", function() {
+	describe("rewrite()", function () {
 
-		it("should pass URL into replacer function when URLs are surrounded by double quotes", function() {
-			var rewriter = new URLRewriter(function(url) {
+		it("should pass URL into replacer function when URLs are surrounded by double quotes", function () {
+			var rewriter = new URLRewriter(function (url) {
 				assert.equal(url, "foo.css");
 				return "bar.css";
 			});
@@ -49,8 +49,8 @@ describe("URLRewriter", function() {
 			assert.equal(result, "@import url(bar.css) screen;");
 		});
 
-		it("should pass URL into replacer function when URLs are surrounded by single quotes", function() {
-			var rewriter = new URLRewriter(function(url) {
+		it("should pass URL into replacer function when URLs are surrounded by single quotes", function () {
+			var rewriter = new URLRewriter(function (url) {
 				assert.equal(url, "foo.css");
 				return "bar.css";
 			});
@@ -59,8 +59,8 @@ describe("URLRewriter", function() {
 			assert.equal(result, "@import url(bar.css) screen;");
 		});
 
-		it("should pass URL into replacer function when URLs are not surrounded quotes", function() {
-			var rewriter = new URLRewriter(function(url) {
+		it("should pass URL into replacer function when URLs are not surrounded quotes", function () {
+			var rewriter = new URLRewriter(function (url) {
 				assert.equal(url, "foo.css");
 				return "bar.css";
 			});
@@ -69,8 +69,8 @@ describe("URLRewriter", function() {
 			assert.equal(result, "@import url(bar.css) screen;");
 		});
 
-		it("should pass URL into replacer function when URLs are surrounded by whitespace", function() {
-			var rewriter = new URLRewriter(function(url) {
+		it("should pass URL into replacer function when URLs are surrounded by whitespace", function () {
+			var rewriter = new URLRewriter(function (url) {
 				assert.equal(url, "foo.css");
 				return "bar.css";
 			});
@@ -79,8 +79,8 @@ describe("URLRewriter", function() {
 			assert.equal(result, "@import url(bar.css) screen;");
 		});
 
-		it("should replace URLs in output when URLs are surrounded by double quotes", function() {
-			var rewriter = new URLRewriter(function() {
+		it("should replace URLs in output when URLs are surrounded by double quotes", function () {
+			var rewriter = new URLRewriter(function () {
 				return "bar.css";
 			});
 
@@ -88,8 +88,8 @@ describe("URLRewriter", function() {
 			assert.equal(result, "@import url(bar.css) screen;");
 		});
 
-		it("should replace URLs in output when URLs are surrounded by single quotes", function() {
-			var rewriter = new URLRewriter(function() {
+		it("should replace URLs in output when URLs are surrounded by single quotes", function () {
+			var rewriter = new URLRewriter(function () {
 				return "bar.css";
 			});
 
@@ -97,8 +97,8 @@ describe("URLRewriter", function() {
 			assert.equal(result, "@import url(bar.css) screen;");
 		});
 
-		it("should replace URLs in output when URLs aren't surrounded by quotes", function() {
-			var rewriter = new URLRewriter(function() {
+		it("should replace URLs in output when URLs aren't surrounded by quotes", function () {
+			var rewriter = new URLRewriter(function () {
 				return "bar.css";
 			});
 
@@ -106,8 +106,8 @@ describe("URLRewriter", function() {
 			assert.equal(result, "@import url(bar.css) screen;");
 		});
 
-		it("should maintain line endings when \\n is used", function() {
-			var rewriter = new URLRewriter(function() {
+		it("should maintain line endings when \\n is used", function () {
+			var rewriter = new URLRewriter(function () {
 				return "bar.css";
 			});
 
@@ -115,8 +115,8 @@ describe("URLRewriter", function() {
 			assert.equal(result, "@import url(bar.css) screen;\n@import url(bar.css);");
 		});
 
-		it("should maintain line endings when \\r\\n is used", function() {
-			var rewriter = new URLRewriter(function() {
+		it("should maintain line endings when \\r\\n is used", function () {
+			var rewriter = new URLRewriter(function () {
 				return "bar.css";
 			});
 
@@ -124,8 +124,8 @@ describe("URLRewriter", function() {
 			assert.equal(result, "@import url(bar.css) screen;\r\n@import url(bar.css);");
 		});
 
-		it("should maintain formatting when comments are present", function() {
-			var rewriter = new URLRewriter(function() {
+		it("should maintain formatting when comments are present", function () {
+			var rewriter = new URLRewriter(function () {
 				return "bar.css";
 			});
 
@@ -133,8 +133,8 @@ describe("URLRewriter", function() {
 			assert.equal(result, "/*import something*/\n@import url(bar.css) screen;");
 		});
 
-		it("should replace URLs when there are two URLs on the same line", function() {
-			var rewriter = new URLRewriter(function() {
+		it("should replace URLs when there are two URLs on the same line", function () {
+			var rewriter = new URLRewriter(function () {
 				return "bar.css";
 			});
 
@@ -142,6 +142,45 @@ describe("URLRewriter", function() {
 			assert.equal(result, fs.readFileSync("./tests/fixtures/minified-after.css", "utf8"));
 		});
 
+		describe("Surround link with quotes", function () {
+
+			it("single quotes: should replace URLs when there are more than one URL in the file", function () {
+				var rewriter = new URLRewriter(function () {
+					return "bar.css";
+				});
+	
+				var result = rewriter.rewrite(fs.readFileSync("./tests/fixtures/before.css", "utf8"), "'");
+				assert.equal(result, fs.readFileSync("./tests/fixtures/after-single-quotes.css", "utf8"));
+			});
+
+			it("double quotes: should replace URLs when there are more than one URL in the file", function () {
+				var rewriter = new URLRewriter(function () {
+					return "bar.css";
+				});
+	
+				var result = rewriter.rewrite(fs.readFileSync("./tests/fixtures/before.css", "utf8"), '"');
+				assert.equal(result, fs.readFileSync("./tests/fixtures/after-double-quotes.css", "utf8"));
+			});
+
+			it("single quotes: should replace URLs when there are two URLs on the same line", function () {
+				var rewriter = new URLRewriter(function () {
+					return "bar.css";
+				});
+	
+				var result = rewriter.rewrite(fs.readFileSync("./tests/fixtures/minified-before.css", "utf8"), "'");
+				assert.equal(result, fs.readFileSync("./tests/fixtures/minified-after-single-quotes.css", "utf8"));
+			});
+
+			it("single quotes: should replace URLs when there are two URLs on the same line", function () {
+				var rewriter = new URLRewriter(function () {
+					return "bar.css";
+				});
+	
+				var result = rewriter.rewrite(fs.readFileSync("./tests/fixtures/minified-before.css", "utf8"), '"');
+				assert.equal(result, fs.readFileSync("./tests/fixtures/minified-after-double-quotes.css", "utf8"));
+			});
+
+		});
 
 	});
 
